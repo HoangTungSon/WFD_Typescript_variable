4443528d1a6793fad047ff333991cddeb7951d1e
hoang tung son <hoangtungson164@gmail.com>
2019-10-21 07:49:30 +0700

    first commit

---
+++ test.js
@@ -0,0 +1,12 @@
+function main() {
+    var y = 0;
+    var x = 5;
+    console.log(x);
+    var x = 10;
+    console.log(x);
+    y = 55;
+    console.log(y);
+    y = 100;
+    console.log(y);
+}
+main();
